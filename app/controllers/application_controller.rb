1a73e0706ddf640364ea61e3a9474b8fbb7ea0e7
Josh Comeau <joshwcomeau@gmail.com>
2014-04-15 16:21:54 -0400

    Added authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,13 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-
   helper_method :current_user
 
+  def ensure_logged_in
+    unless current_user
+      flash[:alert] = "Please log in first!"
+      redirect_to new_session_path
+    end
+  end
 
 end
